e34b297713f5924e420492df9a2f738b7518840b
Paddy <pinky@fedora.fritz.box>
2023-12-19 22:19:15 +0100

    Challenge #7

---
+++ contracts/DAY_1_OF_12_CODEXMAS.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.22;
+
+contract LW3_ChristmasSmartContract_1 {
+
+    function getText() external pure returns(string memory) {
+        return "Ho, Ho, Ho, Merry Christmas!";
+    }
+}
+
+// Ho, Ho, Ho, Merry Christmas! to LearnWeb3 Team
